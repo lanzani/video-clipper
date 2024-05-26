2f275363abbc48f413d0579afb0dedf5ada7a732
Federico Lanzani <federico@federicolanzani.com>
2024-05-26 23:40:19 +0200

    init

--- video-dataset-tools/main.py
+++ video-dataset-tools/main.py
@@ -4,6 +4,19 @@ from moviepy.editor import VideoFileClip
 
 input_video_folder = "input_videos"
 output_video_folder = "output_videos"
+target_fps = 15
+
+def display_video_info(clip, target_duration):
+    # Display video information in the sidebar
+    st.sidebar.title("Source Video Information")
+    st.sidebar.write(f"Resolution: {clip.size[0]}x{clip.size[1]}")
+    st.sidebar.write(f"Framerate: {clip.fps}")
+    st.sidebar.write(f"Length: {clip.duration}")
+
+    st.sidebar.title("Target Video Information")
+    st.sidebar.write(f"Framerate: {target_fps}")
+    st.sidebar.write(f"Length: {target_duration}")
+
 
 
 def process_video(video_path, start_time, end_time, video_id):
@@ -15,7 +28,7 @@ def process_video(video_path, start_time, end_time, video_id):
 
     # Write the clipped video to the output folder with a progressive id
     output_path = os.path.join(output_video_folder, f"{video_id}.mp4")
-    subclip.write_videofile(output_path, fps=15)
+    subclip.write_videofile(output_path, fps=target_fps)
 
     # Close the video clips to free up memory
     subclip.close()
@@ -23,6 +36,7 @@ def process_video(video_path, start_time, end_time, video_id):
 
 
 def main():
+
     # List all files in the directory
     files = os.listdir(input_video_folder)
 
@@ -47,22 +61,21 @@ def main():
     range_slider = st.slider('Select a range', 0, video_duration, (0, video_duration))
 
     # Display the length of the range slider
-    st.write(f"Length of range slider: {range_slider[1] - range_slider[0]}")
+    st.write(f"Length (s): {range_slider[1] - range_slider[0]}")
 
     # Clip the video based on the value of the slider
     start_time, end_time = range_slider
 
     # Display the clipped video with autoplay
-    st.video(video_path, start_time=start_time, end_time=end_time, format="video/mp4")
+    st.video(video_path, start_time=start_time, end_time=end_time, format="video/mp4", autoplay=True)
+
+    display_video_info(clip, range_slider[1] - range_slider[0])
 
     # Create a button to save the video
-    if st.button('Save Video'):
+    if st.sidebar.button('Save Video'):
         # Process the current video
         process_video(video_path, start_time, end_time, st.session_state.video_id)
         st.session_state.video_id += 1
-
-    # Create a button to go to the next video
-    if st.button('Next Video'):
         st.session_state.video_index = (st.session_state.video_index + 1) % len(video_files)
         st.experimental_rerun()
 
